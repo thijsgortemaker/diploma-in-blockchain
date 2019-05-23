9444fc6f959c85ad279bdcd415c3da48329018bc
Justin van Meeteren <jusznvanmeeteren@gmail.com>
2019-05-23 14:26:39 +0200

    basic log-in function built

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,22 +1,22 @@
-router.post('/auth', function(req, res) {
+router.post('/auth', function (req, res) {
 
+    // TODO: Install bcrypt
+    // TODO: Add database functionality
+    // TODO: Add real JWT
     let username = req.body.username;
     let password = req.body.username;
 
     // doorzoek database op username match 
     let foundUsn;
-    let foundPw;
+    let foundHash;
 
     // decrypt found password
-    if(true) {
-        res.json(
-            {"response" : "success",
-             "token" : "token"}
-             )
-    } else {
+    if (false) {
         res.status(403);
-        res.json({"response" : "unsuccessfull"});
+        res.json({ "response": "unsuccessfull" });
+        return;
     }
+    res.status(200).json({ "token": "token" });
 })
 
 module.exports = router;
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ let jwt = require('express-jwt');
 let secret = 'thisNeedsToChange';
 
 function getToken(req) {
-    if(req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token') {
+    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Token') {
         return req.headers.authorization.split(' ')[1];
     }
     return null;
@@ -11,10 +11,10 @@ function getToken(req) {
 // This function might need to change in time too.
 var auth = {
     required: jwt({
-      secret: secret,
-      userProperty: 'payload',
-      getToken: getToken
+        secret: secret,
+        userProperty: 'payload',
+        getToken: getToken
     })
-  };
-  
+};
+
 module.exports = auth;
