2984b277953f0a8aa856cdbd19949e2313761683
ozcanseker <ozcanseker28@hotmail.com>
2019-06-13 13:18:11 +0200

    test2

--- src/cloudAgentServer/routes/api/index.js
+++ src/cloudAgentServer/routes/api/index.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 router.get("/", function(req, rsp){
     console.log("Test is working");
-    rsp.end("Test is working. Connected with Cloud Agent server"); 
+    rsp.end("Test is working. Connected with Cloud Agent server");  
 })
 
 module.exports = router;
